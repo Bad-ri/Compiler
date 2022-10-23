b3a8a3acde8e3521aa0139b2d0371e9ec95d0a72
20194841 <20194841@FUE.EDU.EG>
2022-10-23 14:36:32 +0200

    Update GUI.java

--- Compiler/src/View/GUI.java
+++ Compiler/src/View/GUI.java
@@ -11,7 +11,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class GUI extends JFrame {
