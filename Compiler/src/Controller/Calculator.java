7bd664e16333510a5b1dfa507db87cc748952aa7
20194841 <20194841@FUE.EDU.EG>
2022-10-26 19:48:07 +0200

    Update Calculator.java

--- Compiler/src/Controller/Calculator.java
+++ Compiler/src/Controller/Calculator.java
@@ -6,28 +6,39 @@ import java.util.regex.Pattern;
 
 public class Calculator {
     public static String Calc(String y){
-        String Target ="ID";
-        String Result ="";
-        String FinalResult ="";
+        String Target ="ID"; // replacment of the character and string
+        String Target2 ="x*x"; // replacment of character multiply by character
+        String Result =""; // variable used in the replacment section
+        String FinalResult =""; // final result that will be returned
         String x = "";
         String z = "";
         int Counter = 0 ;
-    Pattern patterntest = Pattern.compile("[0-9][a-z||A-Z]*", Pattern.CASE_INSENSITIVE);
-    Matcher matchertest = patterntest.matcher(y);
-   while(matchertest.find()) {
-           FinalResult="Syntax Error";  
-           break;
+        
+        // if string made from 3 or more characters was found replace it with one character
+        Pattern pattern1 = Pattern.compile("[a-z||A-Z][a-z||A-Z]([a-z||A-Z])+", Pattern.CASE_INSENSITIVE);
+        Matcher matcher1 = pattern1.matcher(y);
+        while(matcher1.find()) {
+           Result = matcher1.replaceAll("x"); 
+        } 
+    if(Result==""){
+       Result=y;
         }
-   
-    Pattern pattern = Pattern.compile("[a-z||A-Z]*[_||.]*[a-z||A-Z]+", Pattern.CASE_INSENSITIVE);
-    Matcher matcher = pattern.matcher(y);
-   while(matcher.find()) {
+        // if string made from 2 characters was found replace it with two characters separated with multiplication symbol in the middle   
+        Pattern pattern2 = Pattern.compile("[a-z||A-Z][a-z||A-Z]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher2 = pattern2.matcher(Result);
+        while(matcher2.find()) {
+           Result = matcher2.replaceAll(Target2);  
+        }
+        // finaly replace all characters with the target (id)    
+        Pattern pattern = Pattern.compile("[a-z||A-Z]+", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(Result);
+        while(matcher.find()) {
            Result = matcher.replaceAll(Target);  
         }
+        
+        // replace the string(ID) with (id) with its count          
    for(int i = 0 ; i<Result.length() ; i++){ 
-       if(FinalResult=="Syntax Error"){
-           break;
-       }
+       
        if(i+2 >= Result.length()){
           x = Result.substring(i, Result.length());
        }
@@ -43,19 +54,17 @@ public class Calculator {
          z=x.substring(1);
          if(z.equals("I")||z.equals("D")){
          }
+         // Error handel when found (_)
          else if(z.equals("_")){
          FinalResult="Syntax Error"; 
          break;
-       }
-        
-         
+       }         
          else{
            FinalResult+=z;      
           }
        }  
    }
-        return FinalResult;
-   
+        return FinalResult; 
     }
 }
 // "[a-z||A-Z]*[_||.]*[a-z||A-Z]+"  Read any String and any alpha
